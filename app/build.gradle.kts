53db9e9d7ca2ea35f9898945c6b4d7cbe98c5c21
awoadak-glitch <awoadak@gmail.com>
2026-09-10 13:14:35 -0700

    Set Anime Witcher hybrid version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.awr.streamhub"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "4.0.0-hitv-polished-api-player"
+        versionCode = 5
+        versionName = "5.0.0-anime-witcher-hybrid"
     }
 
     buildTypes {
